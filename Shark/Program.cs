428897a5941d489b242f3d26e315b53b9edf7722
Tuta <frolkovanasta@yandex.ru>
2024-05-26 22:15:35 +0300

    Добавлен класс Group

--- Shark/Program.cs
+++ Shark/Program.cs
@@ -9,15 +9,25 @@ namespace Shark
         public static void Main(string[] args)
         {
             Course[] courses = { new Course(1), new Course(2), new Course(3), new Course(4) };
+            List<Group> groups = new List<Group>();
             List<Student> students = new List<Student>();
             List<Teacher> teachers = new List<Teacher>();
             List<Discipline> disciplines = new List<Discipline>();
             
-            students.Add(new Student("Рудоков", "Даниил", courses[0]));
-            students.Add(new Student("Куликова", "Мария", courses[1]));
-            students.Add(new Student("Архипова", "Ева", courses[2]));
-            students.Add(new Student("Аксенов", "Роман", courses[3]));
-            students.Add(new Student("Воробьев", "Эмиль", courses[0]));
+            students.Add(new Student("Рудоков", "Даниил", courses[0], courses[0].NumberOfCourse*100+3));
+            students.Add(new Student("Куликова", "Мария", courses[1], courses[1].NumberOfCourse*100+2));
+            students.Add(new Student("Архипова", "Ева", courses[2], courses[2].NumberOfCourse*100+3));
+            students.Add(new Student("Аксенов", "Роман", courses[3], courses[3].NumberOfCourse*100+4));
+            students.Add(new Student("Воробьев", "Эмиль", courses[0], courses[0].NumberOfCourse*100+1));
+            
+            //groups.Add(new Group(101, courses[0], students));
+            for (var i = 1; i < 5; i++)
+            {
+                groups.Add(new Group(i*100+1, courses[i-1], students));
+                groups.Add(new Group(i*100+2, courses[i-1], students));
+                groups.Add(new Group(i*100+3, courses[i-1], students));
+                groups.Add(new Group(i*100+4, courses[i-1], students));
+            }
             
             teachers.Add(new Teacher("Волкова", "Анастасия"));
             teachers.Add(new Teacher("Новикова", "Екатерина"));
@@ -37,7 +47,7 @@ namespace Shark
             {
                 Console.Clear();
                 Console.WriteLine("Выбирете команду:");
-                Console.Write("1 - Студенты\n2 - Преподаватели\n3 - Дисциплина\n0 - Выход из программы\n");
+                Console.Write("1 - Студенты\n2 - Преподаватели\n3 - Дисциплина\n4 - Группы\n0 - Выход из программы\n");
                 Console.Write("Команда: ");
                 var choice = Convert.ToInt32(Console.ReadLine());
                 
@@ -72,7 +82,10 @@ namespace Shark
                                 var name = Console.ReadLine();
                                 Console.Write("Какой курс: ");
                                 var whichCourse = Convert.ToInt32(Console.ReadLine());
-                                students.Add(new Student(surname, name, courses[whichCourse - 1]));
+                                Console.WriteLine("Какая группа: ");
+                                var whichGroup = Convert.ToInt32(Console.ReadLine());
+                                students.Add(new Student(surname, name, courses[whichCourse - 1], whichGroup));
+                                
                                 foreach (var t in disciplines)
                                 {
                                     if (t.NumberOfCourse == whichCourse)
@@ -82,8 +95,13 @@ namespace Shark
                                 }
 
                                 var lastItem = students.LastOrDefault();
+                                /*var right = false;
+                                foreach (var gr in groups)
+                                {
+                                    right = gr.NumberOfGroup == whichGroup; // break or continue
+                                }*/
 
-                                if (lastItem != null && lastItem.LastName == surname && lastItem.FirstName == name && lastItem.Course.NumberOfCourse == whichCourse)
+                                if (lastItem != null && lastItem.LastName == surname && lastItem.FirstName == name && lastItem.Course.NumberOfCourse == whichCourse && lastItem.NumberOfGroup == whichGroup)
                                 {
                                     Console.WriteLine("Успешно добавился!");
                                 }
@@ -208,6 +226,40 @@ namespace Shark
                         }
                         break;
                     }
+                    case 4:
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Выбирете команду:");
+                        Console.Write("1 - Просмотреть информацию о группах\n0 - Вернуться в начальное меню\n");
+                        Console.Write("Команда: ");
+                        choice = Convert.ToInt32(Console.ReadLine());
+
+                        switch (choice)
+                        {
+                            case 1:
+                            {
+                                Console.WriteLine("Выбирете номер группы:");
+                                Console.Write("Группа: ");
+                                var groupeNumber = Convert.ToInt32(Console.ReadLine());
+
+                                foreach (var gr in groups)
+                                {
+                                    if (gr.NumberOfGroup == groupeNumber)
+                                    {
+                                        gr.GetInfo();
+                                    }
+                                }
+                                break;
+                            }
+                            case 0: break;
+                            default:
+                            {
+                                Console.WriteLine("Что-то пошло не так...");
+                                break;
+                            }
+                        }
+                        break;
+                    }
                     case 0:
                     {
                         working = false;
@@ -246,16 +298,79 @@ namespace Shark
     public class Student : User
     {
         public readonly Course Course;
+        public readonly int NumberOfGroup;
 
-        public Student(string lastName, string firstName, Course course)
+        public Student(string lastName, string firstName, Course course, int numberOfGroup)
             : base(lastName, firstName)
         {
             Course = course;
+            switch (course.NumberOfCourse)
+            {
+                case 1:
+                {
+                    switch (numberOfGroup)
+                    {
+                        case 101:
+                        case 102:
+                        case 103:
+                        case 104:
+                            NumberOfGroup = numberOfGroup;
+                            break;
+                    }
+
+                    break;
+                }
+                
+                case 2:
+                {
+                    switch (numberOfGroup)
+                    {
+                        case 201:
+                        case 202:
+                        case 203:
+                        case 204:
+                            NumberOfGroup = numberOfGroup;
+                            break;
+                    }
+
+                    break;
+                }
+                
+                case 3:
+                {
+                    switch (numberOfGroup)
+                    {
+                        case 301:
+                        case 302:
+                        case 303:
+                        case 304:
+                            NumberOfGroup = numberOfGroup;
+                            break;
+                    }
+
+                    break;
+                }
+                
+                case 4:
+                {
+                    switch (numberOfGroup)
+                    {
+                        case 401:
+                        case 402:
+                        case 403:
+                        case 404:
+                            NumberOfGroup = numberOfGroup;
+                            break;
+                    }
+
+                    break;
+                }
+            }
         }
 
         public void GetInfo()
         {
-            Console.WriteLine($"\nФамилия: {LastName}\nИмя: {FirstName}\nКурс: {Course.NumberOfCourse}");
+            Console.WriteLine($"\nФамилия: {LastName}\nИмя: {FirstName}\nКурс: {Course.NumberOfCourse}\nГруппа: {NumberOfGroup}");
         }
     }
 
@@ -304,6 +419,105 @@ namespace Shark
         public Course(int numberOfCourse)
         {
             NumberOfCourse = numberOfCourse;
+        }
+    }
+
+    public class Group
+    {
+        public readonly int NumberOfGroup;
+        public Course Course;
+        private readonly List<Student> _students; 
+
+        public Group(int numberOfGroup, Course course, List<Student> students)
+        {
+            Course = course;
+            _students = students;
+            
+            switch (course.NumberOfCourse)
+            {
+                case 1:
+                {
+                    switch (numberOfGroup)
+                    {
+                        case 101:
+                        case 102:
+                        case 103:
+                        case 104:
+                            NumberOfGroup = numberOfGroup;
+                            break;
+                        default:
+                            Console.WriteLine("Неизвестная группа");
+                            break;
+                    }
+
+                    break;
+                }
+                
+                case 2:
+                {
+                    switch (numberOfGroup)
+                    {
+                        case 201:
+                        case 202:
+                        case 203:
+                        case 204:
+                            NumberOfGroup = numberOfGroup;
+                            break;
+                        default:
+                            Console.WriteLine("Неизвестная группа");
+                            break;
+                    }
+
+                    break;
+                }
+                
+                case 3:
+                {
+                    switch (numberOfGroup)
+                    {
+                        case 301:
+                        case 302:
+                        case 303:
+                        case 304:
+                            NumberOfGroup = numberOfGroup;
+                            break;
+                        default:
+                            Console.WriteLine("Неизвестная группа");
+                            break;
+                    }
+
+                    break;
+                }
+                
+                case 4:
+                {
+                    switch (numberOfGroup)
+                    {
+                        case 401:
+                        case 402:
+                        case 403:
+                        case 404:
+                            NumberOfGroup = numberOfGroup;
+                            break;
+                        default:
+                            Console.WriteLine("Неизвестная группа");
+                            break;
+                    }
+
+                    break;
+                }
+            }
+        }
+
+        public void GetInfo()
+        {
+            foreach (var stu in _students)
+            {
+                if (stu.NumberOfGroup == NumberOfGroup)
+                {
+                    Console.WriteLine($"Фамилия: {stu.LastName}\nИмя: {stu.FirstName}");
+                }
+            }
         }
     }
     
